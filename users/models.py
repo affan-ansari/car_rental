adc9cb40cb9f1a61c00ece2b11cec61fe367bd0a
affan_ansari <affanarif74@gmail.com>
2020-12-23 01:06:53 +0500

    fresh db, profile fixed

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from PIL import Image
 
 class Profile(models.Model):
@@ -9,5 +11,14 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
+    def save(self,**kwargs):
         super().save()
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
