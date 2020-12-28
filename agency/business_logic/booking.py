d9409ea012e499a8ef93e851166a596273ef39ab
Ali Asghar <aliasghar1070@gmail.com>
2020-12-28 15:40:36 +0500

    go go go

--- agency/business_logic/booking.py
+++ agency/business_logic/booking.py
@@ -1,4 +1,15 @@
+from ..models import BOOKING
 class Booking:
-    def __init__(self,allocated_car,allocated_driver,start_date_time,end_date_time,pickup_location,is_driver_needed):
-        pass
+    def __init__(self,allocated_car,allocated_driver,start_date_time,end_date_time,pickup_location,is_driver_needed,customer):
+        new_booking = BOOKING(
+            allocated_car=allocated_car,
+            allocated_driver=allocated_driver,
+            start_date_time=start_date_time,
+            end_date_time=end_date_time,
+            pickup_location=pickup_location,
+            is_driver_needed=is_driver_needed,
+            customer=customer
+            )
+        return new_booking
+
     #Please visit models.py to see Class.
--- agency/business_logic/booking_log.py
+++ agency/business_logic/booking_log.py
@@ -61,15 +61,7 @@ class BookingLog:
             print("Raising exception!")
             raise Exception('Car booked within these dates!')
         else:
-            new_booking = BOOKING (
-            allocated_car=allocated_car,
-            allocated_driver=allocated_driver,
-            start_date_time=start_date_time,
-            end_date_time=end_date_time,
-            pickup_location=pickup_location,
-            is_driver_needed=is_driver_needed,
-            customer=customer
-            )
+            new_booking = Booking(allocated_car,allocated_driver,start_date_time,end_date_time,pickup_location,is_driver_needed,customer)
             new_booking.save()
             return new_booking
 
